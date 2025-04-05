91e6baa1d58c8433893a86dff974a5bde7385e90
elikhabaz <elahe.khabaz.bavani@gmail.com>
2025-04-06 01:11:36 +0330

    add Auth Layout to My blog

---
+++ tildaBlog.Web/Pages/Auth/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tildaBlog.Web.Pages.Auth
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ tildaBlog.Web/Pages/Auth/Register.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tildaBlog.Web.Pages.Auth
+{
+    public class RegisterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
